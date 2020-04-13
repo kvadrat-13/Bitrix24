af997d36b2ab8787df32aac77a950cb7ed170604
Walikakar <wali_kakar22@yahoo.com>
2020-04-13 16:34:45 -0400

    first commit

---
+++ src/test/java/test/AC01.java
@@ -0,0 +1,10 @@
+package test;
+
+public class AC01 {
+
+
+
+       //US:  11. As a PC user, I should be able to download Desktop version of the application.
+
+    //AC: 1. User should be able to click on Mac Os icon under Desktop Client to downloan Mac version of Bitrix.
+}
